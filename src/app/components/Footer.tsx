1e2b9c5ed8223ea5995d5ddef6271b6286630e9c
ionutpetru4046 <ionut4046@yahoo.com>
2025-10-31 23:34:31 +0200

    fixing footer links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
         </div>
 
         {/* Navigation */}
-        <nav className="flex flex-col space-y-3 justify-center">
+        <nav className="flex flex-col space-y-3">
           <h3 className="text-2xl font-bold mb-2">Quick Links</h3>
           <a
             href="#"
